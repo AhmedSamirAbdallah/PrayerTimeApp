bd8eee24e17be45053782541b242bff0ec1fff30
Ahmed Samir <ahmed.samir@itqansystems.com>
2024-04-06 01:20:59 +0200

    [add] country info data

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 let cache = {}
+let cachedResponse;
 
 function getAccessToken() {
     return new Promise((resolve, reject) => {
@@ -116,15 +117,15 @@ function ToggleshowTimings(response = {}, behaviour) {
         { id: "sunrise-card", name: "Sunrise" },
         { id: "zhuhr-card", name: "Dhuhr" },
         { id: "asr-card", name: "Asr" },
-        { id: "maghrib-card", name: "Maghrib"},
-        { id: "isha-card", name: "Isha"},
+        { id: "maghrib-card", name: "Maghrib" },
+        { id: "isha-card", name: "Isha" },
     ]
-    prayers.forEach(prayer=>{
-        if(behaviour=="show"){
+    prayers.forEach(prayer => {
+        if (behaviour == "show") {
             document.getElementById(prayer.id).removeAttribute("hidden");
             document.getElementById(prayer.id).innerHTML = `<h2>${prayer.name}</h2><h3>${response.timings[prayer.name]}</h3>`
         }
-        else{
+        else {
             document.getElementById(prayer.id).setAttribute("hidden", true);
         }
     })
@@ -144,20 +145,22 @@ function getPrayerTime(country, city) {
         })
 }
 
-let country = ""
 getAccessToken()
     .then((response) => getCountries(response.data.auth_token))
     .then((response) => onChangeCountry(response))
     .then((response) => getStates(response))
     .then((response) => {
+        cachedResponse = response
         document.getElementById("country-select").addEventListener('change', (event) => {
+            document.getElementById("country-info").innerHTML = ""
             document.getElementById("city-select").innerHTML = "";
             ToggleshowTimings("hide")
             response.country = event.target.value
-            country = event.target.value
             getStates(response)
         })
     })
 document.getElementById("city-select").addEventListener('change', (event) => {
-    getPrayerTime(country, event.target.value)
+    let countryInfo = document.getElementById("country-info")
+    countryInfo.innerHTML = `<h1>${cachedResponse.country} ,</h1><h1>${event.target.value}</h1>`
+    getPrayerTime(cachedResponse.country, event.target.value)
 })
